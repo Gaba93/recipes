2324ac26ae63143ba452bb73f10986d307c0c8f3
Gaba9 <gaba93@windowslive.com>
2018-08-25 14:11:35 +0200

    Lombok refactoring

--- src/main/java/com/example/recipes/services/RecipeServiceImpl.java
+++ src/main/java/com/example/recipes/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.example.recipes.services;
 
 import com.example.recipes.domain.Recipe;
 import com.example.recipes.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes(){
+        log.debug("I'm in a service!");
         Set<Recipe> recipes = new HashSet<>();
 
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
